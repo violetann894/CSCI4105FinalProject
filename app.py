d4627c0c87c5597a9156d50cb1d1514b56ac505a
violetann894 <rachelhuss2976@gmail.com>
2026-04-27 23:09:02 -0400

    Tweaked somethings

--- app.py
+++ app.py
@@ -48,33 +48,42 @@ dataframe[['Unvaccinated Rate', 'Vaccinated Rate', 'Boosted Rate']] = (
     imputer.transform(dataframe[['Unvaccinated Rate', 'Vaccinated Rate', 'Boosted Rate']]))
 
 # Used AI to assist with picking age ranges that would best fit the data
+# Creates a dictionary that holds the encoding for the ages
 age_dictionary = {'0-4': 0, '5-11': 1, '12-17': 2, '18-29': 3, '30-49': 4, '50-64': 5, '65-79': 6, '80+': 7}
 
+# Creates the reversed age dictionary in {encoded_value, key} format
 reversed_age_dictionary = {}
 for key, value in age_dictionary.items():
     reversed_age_dictionary[value] = key
 
+# Encodes the age groups for analysis
 dataframe['Age Group Encoded'] = dataframe['Age Group'].map(age_dictionary)
 dataframe['Age Group Unencoded'] = dataframe['Age Group Encoded'].map(reversed_age_dictionary)
 
 # Used AI to assist with picking bins that would best fit these features
+# Creates bins to fit the data into that can then be encoded for analysis
 unvaccinated_bins = [-1, 0, 5, 30, 150, np.inf]
 unvaccinated_labels = ['Zero', 'Very Low', 'Low', 'Medium', 'High']
 
+# Creates bins to fit the data into that can then be encoded for analysis
 vaccinated_bins = [-1, 0, 2, 15, 80, np.inf]
 vaccinated_labels = ['Zero', 'Very Low', 'Low', 'Medium', 'High']
 
+# Creates bins to fit the data into that can then be encoded for analysis
 booster_bins = [-1, 0, 3, 10, 60, np.inf]
 booster_labels = ['Zero', 'Very Low', 'Low', 'Medium', 'High']
 
+# Places the rates into the created bins and replaces the value with the label
 dataframe['Unvaccinated Rate'] = pd.cut(dataframe['Unvaccinated Rate'], bins=unvaccinated_bins, labels=unvaccinated_labels)
 dataframe['Vaccinated Rate'] = pd.cut(dataframe['Vaccinated Rate'], bins=vaccinated_bins, labels=vaccinated_labels)
 dataframe['Boosted Rate'] = pd.cut(dataframe['Boosted Rate'], bins=booster_bins, labels=booster_labels)
 
+# Encodes the string value given into an integer value to be used for analysis
 le = LabelEncoder()
 for column in ['Unvaccinated Rate', 'Vaccinated Rate', 'Boosted Rate']:
     dataframe[column] = le.fit_transform(dataframe[column])
 
+# Creates a sidebar with information that is always displayed, no matter what page is open
 with st.sidebar:
     st.header('About')
     st.write('The goal of this project is to create a visualization application that synthesizes COVID-19 information '
@@ -83,65 +92,79 @@ with st.sidebar:
              'the relationships between the different variables and the possible historical reasons for said trends. '
              'The Generative Artificial Intelligence portion of the project aims to allow users to query the AI with '
              'questions regarding the graphs created by the different techniques.')
-
     st.header('Application Created By ')
     st.write('Group 2: Rachel Hussmann, Jeannine Elmasri, Sophia Milask, Anissa Serafine')
 
+# Creates a tab for the decision tree classifier section
 with tab1:
 
+    # Information about what a decision tree classifier is and how that connects to our chosen dataset
     st.header('Decision Tree Classifier')
     st.write('We created a decision tree classifier from the COVID-19 dataset to create a way to predict '
                  'what class a record may fall into based on the following columns and information: Age group, '
                  'boosted rate, vaccinated rate, and unvaccinated rate. The prediction that the classifier is trying to '
                  'complete is deciding whether the combination of those features would result in a case, hospitalization or a death.')
 
+    # Creates the columns and inputs some information about that data and the model
     column_one, column_two, column_three = st.columns(3)
     column_one.metric('Model Accuracy', '87%')
     column_two.metric('Total Records', dataframe.shape[0])
     column_three.metric('Classes', 3)
 
+    # Creating the features and the outcome dataframes for training/testing
     x = dataframe[['Unvaccinated Rate', 'Vaccinated Rate', 'Boosted Rate', 'Age Group Encoded']]
     y = dataframe['Outcome']
 
+    # Creates the training and testing data for the decision tree
     xtraining_data, xtesting_data, ytraining_data, ytesting_data = sk.train_test_split(x, y, test_size=0.2, random_state=42)
 
+    # Creates the decision tree and trains it on the training data
     decisionTree = tree.DecisionTreeClassifier(criterion='entropy', class_weight='balanced')
     decisionTree = decisionTree.fit(xtraining_data, ytraining_data)
 
+    # Gathering the column names from the input features
     feature = []
     for column in x.columns:
         feature.append(column)
 
+    # Title for this section of the application
     st.subheader('Visualization of the Decision Tree')
 
+    # Explanation about the visual of the decision tree
     st.write('Below is the structure of the classifier and how it makes decisions based on the training set we have '
              'given it. The current makeup of the data is 80% training and 20% testing. The view that can be seen '
              'is halted at three levels, but the maximum depth of the tree is unlimited.')
 
+    # Creates the visual of the decision tree
     decisionTreePlt, ax1 = plt.subplots(figsize=(80, 20), dpi=150)
     tree.plot_tree(decisionTree, feature_names=feature, filled=True, fontsize=25, max_depth=3, ax=ax1)
     st.pyplot(decisionTreePlt)
     plt.close(decisionTreePlt)
 
+    # Gives the decision tree the testing data and saves the predictions
     y_predict = decisionTree.predict(xtesting_data)
 
+    # Explanation for the classification report
     st.subheader('Classification Report')
     st.write('The classification report gives more specific performance metrics associated with the decision tree '
              'classifier. These metrics include precision, recall, f1-score, support and overall accuracy')
 
+    # Creates the visual table of the classification report
     report = classification_report(ytesting_data, y_predict, output_dict=True)
     report_dataframe = pd.DataFrame(report).transpose()
     report_dataframe.index = ['Cases', 'Deaths', 'Hospitalizations', 'Accuracy', 'Macro Avg', 'Weighted Avg']
-
     st.dataframe(report_dataframe, width='stretch')
 
+    # Creates the confusion matrix to see how the decision tree did
     confusion = confusion_matrix(ytesting_data, y_predict)
 
+    # Explanation for the confusion matrix
     st.subheader('Confusion Matrix')
     st.write('The confusion matrix is responsible for showing us what the model classified correctly and incorrectly. '
              'This allowed us to tune specific attributes of the model in a way to improve those areas and in turn '
              'create an overall better performing model. ')
 
+    # Creates the visual output of the confusion matrix
     cm, ax2 = plt.subplots()
     sns.heatmap(confusion, annot=True, fmt='d', cmap='Reds')
     plt.xlabel('Predicted')
@@ -149,15 +172,23 @@ with tab1:
     st.pyplot(cm)
     plt.close(cm)
 
+# Creates a tab for the decision tree prediction section
 with tab2:
+    # Information about the tab and instructions for the user on how to use it
     st.header('Decision Tree Prediction')
     st.write('Using our decision tree from the previous tab, we created an interactive format for users to see what '
              'different values for each of the features affect the prediction of the decision tree. Using the dropdowns '
              'below, choose the various values for each feature. When complete, click continue and the decision tree '
              'will show its prediction.')
 
+    # Creates a container for user input
     with st.form('Prediction Form'):
+
+        # Creates the organizational columns
         column_one, column_two, column_three, column_four = st.columns(4)
+
+        # Within the columns, creates a dropdown box where users can pick from values that match the different bin
+        # labels for each rate
         with column_one:
             unvaccinated_choice = st.selectbox('Unvaccinated Rate', unvaccinated_labels, placeholder='Zero')
         with column_two:
@@ -166,34 +197,51 @@ with tab2:
             boosted_choice = st.selectbox('Boosted Rate', booster_labels, placeholder='Zero')
         with column_four:
             age_group_choice = st.selectbox('Age Group', age_dictionary.keys(), placeholder='0-4')
+
+        # Required for the form to work, gives the user a button to submit their choices
         submit = st.form_submit_button('Predict')
 
+    # Checks to see if the button has been clicked
     if submit:
+
+        # If it has, compile the choices into 2d array of values and give it to the decision tree to predict the outcome
+        # Display the prediction once it is done
         st.subheader('Model Prediction')
         st.write('Decision Tree Prediction: ', decisionTree.predict([[unvaccinated_labels.index(unvaccinated_choice),
                                                                      vaccinated_labels.index(vaccinated_choice),
                                                                      booster_labels.index(boosted_choice),
                                                                      age_dictionary[age_group_choice]]]))
-
+# Creates a tab for the association mining section
 with tab3:
+
+    # Describes what the tab dopes and the instructions for the user
     st.header('Association Mining')
     st.write('For this part of the project, we created an interactive tool that allows you the user to choose what '
              'threshold values you want. We utilized the Apriori algorithm for this part of the project. Please input '
              'the minimum values for support, confidence, and lift. Once you submit your choice, the program will '
              'output what the rules it found were, any supporting values, and the plot at the bottom.')
 
-    # Refactor (also buggy)
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        min_support = st.slider('Minimum Support', 0.01, 1.0, 0.20)
-    with col2:
-        min_confidence = st.slider('Minimum Confidence', 0.01, 1.0, 0.20)
-    with col3:
-        min_lift = st.slider('Minimum Lift', 0.0, 10.0, 1.0)
+    # Creates the organizational columns
+    column_1, column_2, column_3 = st.columns(3)
+    with st.form('Association Mining Form'):
+        # Each column has a slider that allows the user to decide what the minimum value for various metrics should be
+        with column_1:
+            minimum_support = st.slider('Minimum Support', 0.01, 1.0, value=0.2, step=0.01)
+        with column_2:
+            minimum_confidence = st.slider('Minimum Confidence', 0.01, 1.0, value=0.2, step=0.01)
+        with column_3:
+            minimum_lift = st.slider('Minimum Lift', 0.0, 2.0, value=0.0, step=0.5)
 
-    # Refactor (also buggy)
-    if st.button('Mine Association Rules'):
+        # Required for the form to work, gives the user a button to submit their choices
+        submit = st.form_submit_button('Submit Minimum Values')
+
+    # Check if the button to submit the values has been clicked
+    if submit:
+
+        # Created an empty list to hold the finalized transaction
         transactions = []
+
+        # Used AI to generate the loop and the nice formatting for the values
         for _, row in dataframe.iterrows():
             transaction = [
                 f'Outcome_{row["Outcome"]}',
@@ -204,16 +252,20 @@ with tab3:
             ]
             transactions.append(transaction)
 
-
+        # Creates the transaction encoder that will take the transactions and turn them into something the apriori
+        # algorithm can work with
         te = TransactionEncoder()
         te_array = te.fit(transactions).transform(transactions)
         dataframe_encoded = pd.DataFrame(te_array, columns=te.columns_)
 
-        frequent_itemsets = apriori(dataframe_encoded, min_support=min_support, use_colnames=True)
-        rules = association_rules(frequent_itemsets, metric='confidence', min_threshold=min_confidence)
+        # Uses the apriori algorithm to find the frequent itemsets from the encoded dataframe
+        frequent_itemsets = apriori(dataframe_encoded, min_support=minimum_support, use_colnames=True)
+
+        # Uses the association rules function to find the association rules based on confidence
+        rules = association_rules(frequent_itemsets, metric='confidence', min_threshold=minimum_confidence)
 
         # Refactor (also buggy)
-        filtered_rules = rules[rules['lift'] >= min_lift].copy()
+        filtered_rules = rules[rules['lift'] >= minimum_lift].copy()
         filtered_rules['antecedents'] = filtered_rules['antecedents'].apply(lambda x: ', '.join(list(x)))
         filtered_rules['consequents'] = filtered_rules['consequents'].apply(lambda x: ', '.join(list(x)))
 
